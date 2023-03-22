4c8189578fb5d6f87848043249cadfde99585646
lucas3g <lucassilva080397@gmail.com>
2023-03-21 21:27:27 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+import time
+
+class Cotacoes:
+    def __init__(self, codigo, nome, cotacao):
+        self.codigo = codigo
+        self.nome = nome
+        self.cotacao = cotacao
+
+print('Executando API. Aguarde...')
+
+# Endpoint da API de cotações
+url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL'
+
+# Faz uma requisição GET para a API
+response = requests.get(url)
+
+# Analisa a resposta da API como um objeto JSON
+data = response.json()
+
+# Lista de objetos Cotacoes
+cotacoes = []
+
+# Loop pelos dados da API e cria objetos Cotacoes
+for codigo, info in data.items():
+    nome = info["name"]
+    cotacao = info["bid"]
+    cotacao_obj = Cotacoes(codigo, nome, cotacao)
+    cotacoes.append(cotacao_obj)
+
+# Imprime as cotações
+for cotacao in cotacoes:
+    print(f"{cotacao.codigo} - {cotacao.nome}: {cotacao.cotacao}")
+ 
+# 60 segunos deixa aplicação parada    
+time.sleep(60)
