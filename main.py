7f627ee994307c87d946229969eb6719a441440d
Lucas Silva <lucassilva080397@gmail.com>
2023-03-22 09:17:18 -0300

    feat: adicionado o tempo que leva a requisição

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ print('Executando API. Aguarde...')
 
 print('--------------------------')
 
+inicio = time.time()
+
 # Endpoint da API de cotações
 url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL'
 
 # Faz uma requisição GET para a API
 response = httpx.get(url)
+
+fim = time.time()
+
+tempo_decorrido = fim - inicio
 
 # Analisa a resposta da API como um objeto JSON
 data = response.json()
@@ -34,6 +40,28 @@ for codigo, info in data.items():
 for cotacao in cotacoes:
     print(f"{cotacao.nome}: {cotacao.cotacao:,.2f}")
     
+print('--------------------------')
+
+# determina a unidade de tempo apropriada
+if tempo_decorrido < 1:
+    unidade = "ms"
+    tempo_decorrido *= 1000
+elif tempo_decorrido < 60:
+    unidade = "s"
+else:
+    unidade = "min"
+    tempo_decorrido /= 60
+
+# formata a saída
+if unidade == "ms":
+    tempo_decorrido_fmt = f"{tempo_decorrido:.2f} {unidade}"
+elif unidade == "s":
+    tempo_decorrido_fmt = f"{tempo_decorrido:.2f} {unidade}"
+else:
+    tempo_decorrido_fmt = f"{tempo_decorrido:.2f} {unidade}"
+
+print("Tempo decorrido:", tempo_decorrido_fmt)
+
 print('--------------------------')
 
 time.sleep(9999)
